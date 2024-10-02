57f3078f26bf87eb93db5486abf758a260efe20a
BHUVANESHWARAN K <bhuvaneshw522@gmail.com>
2024-10-02 21:06:55 +0530

    init commit

---
+++ lib/calculator.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Calculator",
+          style: TextStyle(
+              color: Colors.white, fontSize: 23, fontWeight: FontWeight.bold),
+        ),
+        
+      ),
+    );
+  }
+}
