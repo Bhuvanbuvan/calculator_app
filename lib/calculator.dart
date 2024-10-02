d1ddd59128bdead41d2399a9ccec1db6cdab2835
BHUVANESHWARAN K <bhuvaneshw522@gmail.com>
2024-10-02 22:28:42 +0530

    callculation added

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -8,6 +8,25 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  Widget _calcButton(String textName, Color buttonColor, Color textColor) {
+    return Container(
+      child: ElevatedButton(
+        onPressed: () {
+          calculation(textName);
+        },
+        child: Text(
+          textName,
+          style: TextStyle(color: textColor, fontSize: 35),
+        ),
+        style: TextButton.styleFrom(
+          backgroundColor: buttonColor,
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(10),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +38,202 @@ class _CalculatorState extends State<Calculator> {
           style: TextStyle(
               color: Colors.white, fontSize: 23, fontWeight: FontWeight.bold),
         ),
-        
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    text,
+                    style: TextStyle(fontSize: 100, color: Colors.white),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _calcButton("AC", Colors.grey, Colors.black),
+                _calcButton("+/-", Colors.grey, Colors.black),
+                _calcButton("%", Colors.grey, Colors.black),
+                _calcButton(
+                    "/", const Color.fromARGB(255, 255, 162, 2), Colors.black),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _calcButton("7", Colors.grey.shade800, Colors.white),
+                _calcButton("8", Colors.grey.shade800, Colors.white),
+                _calcButton("9", Colors.grey.shade800, Colors.white),
+                _calcButton(
+                    "*", const Color.fromARGB(255, 255, 162, 2), Colors.black),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _calcButton("4", Colors.grey.shade800, Colors.white),
+                _calcButton("5", Colors.grey.shade800, Colors.white),
+                _calcButton("6", Colors.grey.shade800, Colors.white),
+                _calcButton(
+                    "-", const Color.fromARGB(255, 255, 162, 2), Colors.black),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _calcButton("1", Colors.grey.shade800, Colors.white),
+                _calcButton("2", Colors.grey.shade800, Colors.white),
+                _calcButton("3", Colors.grey.shade800, Colors.white),
+                _calcButton(
+                    "+", const Color.fromARGB(255, 255, 162, 2), Colors.black),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _calcButton("00", Colors.grey.shade800, Colors.white),
+                _calcButton("0", Colors.grey.shade800, Colors.white),
+                _calcButton(".", Colors.grey.shade800, Colors.white),
+                _calcButton(
+                    "=", const Color.fromARGB(255, 255, 162, 2), Colors.black),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  // Calculator logic
+  dynamic text = '0';
+  double numOne = 0;
+  double numTwo = 0;
+
+  dynamic result = '';
+  dynamic finalResult = '';
+  dynamic opr = '';
+  dynamic preOpr = '';
+
+  void calculation(String btnText) {
+    if (btnText == 'AC') {
+      text = '0';
+      numOne = 0;
+      numTwo = 0;
+      result = '';
+      finalResult = '0';
+      opr = '';
+      preOpr = '';
+    } else if (opr == '=' && btnText == '=') {
+      if (preOpr == '+') {
+        finalResult = add();
+      } else if (preOpr == '-') {
+        finalResult = sub();
+      } else if (preOpr == '*') {
+        finalResult = mul();
+      } else if (preOpr == '/') {
+        finalResult = div();
+      }
+    } else if (btnText == '+' ||
+        btnText == '-' ||
+        btnText == '*' ||
+        btnText == '/' ||
+        btnText == '=') {
+      if (numOne == 0) {
+        numOne = double.parse(result);
+      } else {
+        numTwo = double.parse(result);
+      }
+
+      if (opr == '+') {
+        finalResult = add();
+      } else if (opr == '-') {
+        finalResult = sub();
+      } else if (opr == '*') {
+        finalResult = mul();
+      } else if (opr == '/') {
+        finalResult = div();
+      }
+      preOpr = opr;
+      opr = btnText;
+      result = '';
+    } else if (btnText == '%') {
+      result = (numOne / 100).toString();
+      finalResult = doesContainDecimal(result);
+    } else if (btnText == '.') {
+      if (!result.toString().contains('.')) {
+        result = result.toString() + '.';
+      }
+      finalResult = result;
+    } else if (btnText == '+/-') {
+      result.toString().startsWith('-')
+          ? result = result.toString().substring(1)
+          : result = '-' + result.toString();
+      finalResult = result;
+    } else {
+      result = result + btnText;
+      finalResult = result;
+    }
+
+    setState(() {
+      text = finalResult;
+    });
+  }
+
+  String add() {
+    result = (numOne + numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String sub() {
+    result = (numOne - numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String mul() {
+    result = (numOne * numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String div() {
+    result = (numOne / numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String doesContainDecimal(dynamic result) {
+    if (result.toString().contains('.')) {
+      List<String> splitDecimal = result.toString().split('.');
+      if (!(int.parse(splitDecimal[1]) > 0)) {
+        return result = splitDecimal[0].toString();
+      }
+    }
+    return result;
   }
 }
